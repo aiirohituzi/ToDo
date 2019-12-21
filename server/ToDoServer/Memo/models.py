77725de3eeb840c210be701d04f02ca424a104e9
aiirohituzi <sinn5656@gmail.com>
2019-12-21 22:08:05 +0900

    눈에 보이는 기능들을 기반으로 기본적인 데이터 모델 구성

---
+++ server/ToDoServer/Memo/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+
+class Group(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    group_name = models.CharField(max_length=50)
+
+
+class Memo(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, models.SET_NULL, blank=True, null=True)
+    content = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    isDo = models.BooleanField(default=False)
+    isStar = models.BooleanField(default=False)
+    targetDate = models.DateTimeField(null=True, blank=True)
