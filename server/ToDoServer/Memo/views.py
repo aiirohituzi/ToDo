94e084fd3cd79ef675440c9e5eda30a34e10682a
aiirohituzi <sinn5656@gmail.com>
2019-12-23 17:31:58 +0900

    그룹 아이디로 메모 목록 조회하는 뷰 추가

--- server/ToDoServer/Memo/views.py
+++ server/ToDoServer/Memo/views.py
@@ -14,6 +14,7 @@ def get_all_memo(request):
     try:
         for item in Memo.objects.filter(owner=user).order_by('-created_at'):
             data.append({
+                'id': item.id,
                 'owner': item.owner,
                 'group': item.group,
                 'content': item.content,
@@ -42,6 +43,7 @@ def get_all_group(request):
     try:
         for item in Group.objects.filter(owner=user).order_by('-created_at'):
             data.append({
+                'id': item.id,
                 'owner': item.owner,
                 'group_name': item.group,
             })
@@ -50,6 +52,41 @@ def get_all_group(request):
         return HttpResponseServerError()
 
     print("[Get request: All Group]")
+    data = json.dumps(data, indent=4)
+    print(data)
+
+    return HttpResponse(data, content_type="application/json")
+
+
+def get_memo_by_groupId(request):
+    data = []
+    user = request.Get.get('user', None)
+    group = request.Get.get('group', False)
+    # user에 대한 자세한 값은 후에 인증기능 추가 후 알맞게 수정
+    # 임시로 모양만 구현
+    # group 이름을 받든 id를 받든 역시 추후 수정 필요
+
+    if not group:
+        return HttpResponse(status=400)
+
+    try:
+        for item in Memo.objects.filter(Q(user=user) & Q(group=group)).order_by('-created_at'):
+            data.append({
+                'id': item.id,
+                'owner': item.owner,
+                'group': item.group,
+                'content': item.content,
+                'created_at': str(item.created_at),
+                'updated_at': str(item.updated_at),
+                'isDo': item.isDo,
+                'isStar': item.isStar,
+                'targetDate': item.targetDate,
+            })
+    except:
+        print("[Get request: Memo by Group ID] ERROR")
+        return HttpResponseServerError()
+
+    print("[Get request: Memo by Group ID]")
     data = json.dumps(data, indent=4)
     print(data)
 
