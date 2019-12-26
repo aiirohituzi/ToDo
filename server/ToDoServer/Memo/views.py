75fb943b23ea8676ada88e183df6bf6489df95c8
aiirohituzi <sinn5656@gmail.com>
2019-12-26 15:59:10 +0900

    memo 생성 뷰 기본틀 추가, 잡다한 수정

--- server/ToDoServer/Memo/views.py
+++ server/ToDoServer/Memo/views.py
@@ -58,10 +58,10 @@ def get_all_group(request):
     return HttpResponse(data, content_type="application/json")
 
 
-def get_memo_by_groupId(request):
+def get_memo_by_group_id(request, group_id):
+    # memo = get_object_or_404(Memo, group=group_id) 이 방식 괜찮은지 후에 검토
     data = []
     user = request.Get.get('user', None)
-    group = request.Get.get('group', False)
     # user에 대한 자세한 값은 후에 인증기능 추가 후 알맞게 수정
     # 임시로 모양만 구현
     # group 이름을 받든 id를 받든 역시 추후 수정 필요
@@ -70,7 +70,7 @@ def get_memo_by_groupId(request):
         return HttpResponse(status=400)
 
     try:
-        for item in Memo.objects.filter(Q(user=user) & Q(group=group)).order_by('-created_at'):
+        for item in Memo.objects.filter(Q(user=user) & Q(group=group_id)).order_by('-created_at'):
             data.append({
                 'id': item.id,
                 'owner': item.owner,
@@ -91,6 +91,23 @@ def get_memo_by_groupId(request):
     print(data)
 
     return HttpResponse(data, content_type="application/json")
+
+
+def add_memo(request):
+    user = request.POST['user']
+    memo = request.POST['memo']
+    # memo 안에 JSON 오브젝트 형태로 담을 예정
+    # String으로 값이 넘어올 수 있음, 확인 후 변환작업 필요
+
+    ########################
+    # user 검증 코드 들어가야함 #
+    # 아니면 데코레이터로 처리   #
+    ########################
+
+    memo_obj = Memo(owner=user, group=memo.group, content=memo.content,
+                    isDo=memo.isDo, isStar=memo.isStar, targetDate=memo.targetDate)
+    memo_obj.save()
+    return HttpResponse(status=200)
 
 
 def update_memo(request):
