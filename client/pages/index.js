0f992c9dcfd4eefc740c6cb11a4caf6b327cf70b
aiirohituzi <sinn5656@gmail.com>
2020-01-13 21:29:55 +0900

    next.js 기본 사용법 연습

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,19 @@
+import Link from "next/link";
+import Layout from "../components/Layout";
+import Head from "next/head";
+
 const Index = () => (
-  <div>
+  <Layout>
+    <Head>
+      <title>Index 페이지</title>
+    </Head>
     <h1>안녕, Next.js</h1>
-  </div>
+    <h2>
+      <Link href="/about">
+        <div style={{ background: "black", color: "white" }}>소개</div>
+      </Link>
+    </h2>
+  </Layout>
 );
 
 export default Index;
