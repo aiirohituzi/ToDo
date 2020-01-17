4277ffac5e6747cb816a2c85828318477e25e4fa
aiirohituzi <sinn5656@gmail.com>
2020-01-17 16:30:27 +0900

    'https://developer.mozilla.org/en-US/docs/Web/API/Document/drag_event'에 바닐라JS로 구현하는 코드

--- client/pages/dragAnimation.js
+++ client/pages/dragAnimation.js
@@ -2,65 +2,112 @@ import Layout from "../components/Layout";
 import { useState, useEffect } from "react";
 
 const DragAnimation = () => {
-  const down = e => {
-    console.log(e);
-    e.target.style.background = "#fff";
-    e.target.style.position = "fixed";
-    e.target.addEventListener("mousemove", move);
-    // e.target.addEventListener("mouseup", up);
-  };
-
-  const move = e => {
-    console.log(e.target);
-    e.target.style.top = e.clientX - 5;
-    // e.target.style.top = '10vw';
-    e.target.style.left = e.clientX - 5;
-  };
-
-  const up = e => {
-    console.log(e);
-    e.target.style.background = "#f99";
-    e.target.style.removeProperty("position");
-    e.target.style.removeProperty("top");
-    e.target.style.removeProperty("left");
-
-    e.target.removeEventListener("mousemove", move);
-    e.target.removeEventListener("mouseup", up);
-  };
-
   useEffect(() => {
-    console.log(document.querySelector("li"));
-    document.querySelector("li").addEventListener("mousedown", down);
-    // return document.querySelector("li").removeEventListener("mousedown", down);
+    var dragged;
+
+    /* events fired on the draggable target */
+    document.addEventListener("drag", function(event) {}, false);
+
+    document.addEventListener(
+      "dragstart",
+      function(event) {
+        // store a ref. on the dragged elem
+        dragged = event.target;
+        // make it half transparent
+        event.target.style.opacity = 0.5;
+      },
+      false
+    );
+
+    document.addEventListener(
+      "dragend",
+      function(event) {
+        // reset the transparency
+        event.target.style.opacity = "";
+      },
+      false
+    );
+
+    /* events fired on the drop targets */
+    document.addEventListener(
+      "dragover",
+      function(event) {
+        // prevent default to allow drop
+        event.preventDefault();
+      },
+      false
+    );
+
+    document.addEventListener(
+      "dragenter",
+      function(event) {
+        // highlight potential drop target when the draggable element enters it
+        if (event.target.className == "dropzone") {
+          event.target.style.background = "purple";
+        }
+      },
+      false
+    );
+
+    document.addEventListener(
+      "dragleave",
+      function(event) {
+        // reset background of potential drop target when the draggable element leaves it
+        if (event.target.className == "dropzone") {
+          event.target.style.background = "";
+        }
+      },
+      false
+    );
+
+    document.addEventListener(
+      "drop",
+      function(event) {
+        // prevent default action (open as link for some elements)
+        event.preventDefault();
+        // move dragged elem to the selected drop target
+        if (event.target.className == "dropzone") {
+          event.target.style.background = "";
+          dragged.parentNode.removeChild(dragged);
+          event.target.appendChild(dragged);
+        }
+      },
+      false
+    );
   }, []);
+
   return (
     <Layout>
       <style>
         {`
-          ul {
-            list-style: none;
+          #draggable {
+            width: 200px;
+            height: 20px;
+            text-align: center;
+            background: white;
           }
-          li {
-            width: 20vw;
-            margin: 3px;
-            padding: 5px;
-            background: #f99;
-            border: 1px solid #f00;
-            border-radius: 5px;
-            cursor: pointer;
-          }
-          li:hover {
-            background: #f55;
+          
+          .dropzone {
+            width: 200px;
+            height: 20px;
+            background: blueviolet;
+            margin-bottom: 10px;
+            padding: 10px;
           }
         `}
       </style>
-      <ul>
-        <li>List 1</li>
-        <li>List 2</li>
-        <li>List 3</li>
-        <li>List 4</li>
-        <li>List 5</li>
-      </ul>
+      <div class="dropzone">
+        <div
+          id="draggable"
+          draggable="true"
+          ondragstart="event.dataTransfer.setData('text/plain',null)"
+        >
+          This div is draggable
+        </div>
+      </div>
+      <div class="dropzone"></div>
+      <div class="dropzone"></div>
+      <div class="dropzone"></div>
     </Layout>
   );
 };
