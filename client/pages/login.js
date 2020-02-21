1c4a9f46b46855c3868766861d7158522a820724
aiirohituzi <sinn5656@gmail.com>
2020-02-21 17:05:46 +0900

    로그인 api 테스트용 임시 ui 추가

--- client/pages/login.js
+++ client/pages/login.js
@@ -8,24 +8,68 @@ const login = () => {
         {`
           .wrapper-login {
             position: relative;
-            background: #00dddd;
-            min-width: 200px;
-            min-height: 250px;
-            width: 20vw;
-            height: 25vw;
+            background: #ff9000;
+            border-radius: 5px;
+            box-shadow: 10px 10px 5px #555;
+            width: 300px;
+            height: 400px;
             top: 20vh;
             margin: auto;
             text-align: center;
           }
 
-          input[type=text] {
+          .wrapper-login .title {
+            padding-top: 10%;
+            height: 35%;
+          }
+
+          .input-group {
+            width: 80%;
+            margin: auto;
+            margin-bottom: 20px;
+            text-align: right;
+          }
+
+          .input-group span {
+            width: 10%;
+            margin-right: 10px;
+          }
+
+          input#id, #pw {
+            bottom: 0px;
             width: 70%;
+            height: 30px;
+          }
+
+          .button-group {
+            width: 100%;
+          }
+
+          .button-group button{
+            border-radius: 5px;
+            width: 35%;
+            height: 30px;
+            font-size: smaller;
+            margin: 3%;
           }
         `}
       </style>
       <div class="wrapper-login">
-        <input type="text" />
-        <input type="text" />
+        <div class="title">
+          <h2>로그인</h2>
+        </div>
+        <div class="input-group">
+          <span>ID </span>
+          <input id="id" type="text" />
+        </div>
+        <div class="input-group">
+          <span>PW </span>
+          <input id="pw" type="text" />
+        </div>
+        <div class="button-group">
+          <button>로그인</button>
+          <button>회원가입</button>
+        </div>
       </div>
     </Layout>
   );
